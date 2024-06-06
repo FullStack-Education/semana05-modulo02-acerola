8f9bf0bd02d5390206e3050be5c6c61f5dbfcac3
gustavobdambros <gustavo_dambros1@hotmail.com>
2024-06-05 21:26:06 -0300

    Exercício 2 - M2S05

---
+++ scripts.js
@@ -0,0 +1,9 @@
+//[M2S05] Ex. 02 - LabScore (Pt.2)
+let nomeAluno = prompt("Qual o nome do aluno?");
+let idadeAluno = prompt("Qual a idade do aluno?");
+let serieAluno = prompt("Qual a série do aluno?");
+let nomeEscola = prompt("Qual o nome da escola?");
+let materiaFavorita = prompt("Qual a sua matéria favorita?");
+
+// Confirmar os dados inseridos
+let confirmacao = confirm("Confirma os dados inseridos?");
