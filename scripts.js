747512f8ed35a4de62848759a7e4a2cfa1c56bd5
Vitor Schultz <vitorserta@gmail.com>
2024-06-09 17:21:52 -0300

    atualizando média geral com o valor já salvo na página

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-import { adicionarLinha } from './addLinha.js';
+import { adicionarLinha, mediaMaterias } from './addLinha.js';
 import { atualizarMediaGeral } from './mediaGeral.js';
 
 //[M2S05] Ex. 02 - LabScore (Pt.2)
@@ -25,5 +25,13 @@ if (confirmacao) {
 //[M2S05] Ex. 04 - LabScore (Pt.2) & Ex. 05
 document.querySelector('button').addEventListener('click', () => {
   adicionarLinha();
+  atualizarMediaGeral();
+});
+
+//[M2S05] Ex. 06 - LabScore (Pt.2)
+document.addEventListener('DOMContentLoaded', () => {
+  const mediaTexto = document.querySelector('#media-geral').textContent;
+  const mediaInicial = parseFloat(mediaTexto.match(/[\d\.]+/)[0]);
+  mediaMaterias.push(mediaInicial);
   atualizarMediaGeral();
 });
